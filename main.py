f47e1473c05cb0dcb42f86f6fd66cc663cd94b84
https://github.com/yunaaas/futoji-mobile.git <linkov.totosa2@bk.ru>
2024-12-13 21:32:41 +0300

    refactor: states + token remove вовремя..

--- main.py
+++ main.py
@@ -11,19 +11,19 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from event import *
 from bot_instance import bot
 
-class EventState(StatesGroup):
-    waiting_for_event_type = State()
-    waiting_for_event_name = State()
-    waiting_for_event_description = State()
-    waiting_for_workshop_option = State() 
-    waiting_for_workshop_instructor = State() 
-    waiting_for_workshop_max_participants = State() 
-    waiting_for_excel_file = State()
-    waiting_for_more_workshops = State()
-    waiting_for_vote_options = State()
-    waiting_for_workshop_selection = State()
-    waiting_for_participant_name = State()
-    waiting_for_group_number = State()
+# class EventState(StatesGroup):
+#     waiting_for_event_type = State()
+#     waiting_for_event_name = State()
+#     waiting_for_event_description = State()
+#     waiting_for_workshop_option = State() 
+#     waiting_for_workshop_instructor = State() 
+#     waiting_for_workshop_max_participants = State() 
+#     waiting_for_excel_file = State()
+#     waiting_for_more_workshops = State()
+#     waiting_for_vote_options = State()
+#     waiting_for_workshop_selection = State()
+#     waiting_for_participant_name = State()
+#     waiting_for_group_number = State()
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
